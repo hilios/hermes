f3eccf4e572c7982bb8a2003afbc1c48c2063e3f
Edson Hilios <ehilios@corp.hotwords.com>
2012-05-03 09:54:07 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hermes::Application.config.secret_token = '740b5a2c8730f618322bbdc0638579e90d2f554ad45b865d5c75958f0d6230eceafad7546fc84321079d715b113757f401eee4313ec95aafefa9c8a9b3d42db4'
