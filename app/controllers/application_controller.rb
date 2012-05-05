d8790e2937045e65a359db2fa3a8ebf948d8ee15
Edson Hilios <edson.hilios@gmail.com>
2012-05-05 12:58:48 -0300

    Adding the authentication requirement to the app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery
+  
   respond_to :html
 
-  protect_from_forgery
-
+  before_filter :authenticate_user!
+  
   layout :layout_by_resource
 
   def layout_by_resource
