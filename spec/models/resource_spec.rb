fa37738c5029f53f294c9457743023fb5875374c
Edson Hilios <ehilios@corp.hotwords.com>
2012-05-23 17:10:48 -0300

    Removed the is_public field from resource; Implemented uri, url and urn methods on resource class; method to test if resource is a folder

--- spec/models/resource_spec.rb
+++ spec/models/resource_spec.rb
@@ -2,9 +2,7 @@ require 'spec_helper'
 
 describe Resource do
   describe "collection" do
-    it { should have_field(:is_public).of_type(Boolean) }
     it { should have_field(:uri).of_type(String) }
-    it { should have_field(:asset_type).of_type(String) }
     
     describe "modules" do
       it { should have_field(:version).of_type(Integer) }
@@ -19,5 +17,48 @@ describe Resource do
   
   describe "relations" do
     it { should embed_one(:asset) }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:uri) }
+    it { should validate_presence_of(:asset) }
+  end
+
+  describe "mass assigment" do
+    it { should_not allow_mass_assignment_of(:uri) }
+  end
+
+  describe "callbacks" do
+    it "calls the #uri method before save" do
+      Resource.any_instance.stub(:uri) { "/path/to/file" }
+      Resource.any_instance.should_receive(:uri).at_least(:once)
+      FactoryGirl.create(:resource)
+    end
+  end
+
+  let(:folder)      { FactoryGirl.create(:folder)    }
+  let(:sub_folder)  { FactoryGirl.create(:folder, :parent => folder)  }
+
+  describe "#url" do
+    it "returns a slash if is root" do
+      folder.url.should == "/"
+    end
+
+    it "returns a string with the parents uri with a slash at the end" do
+      sub_folder.url.should =~ Regexp.new(sub_folder.parent.uri)
+    end
+  end
+
+  describe "#uri" do
+    it "builds the uri string with the parent " do
+      folder.uri.should =~ Regexp.new(folder.url)
+      folder.uri.should =~ Regexp.new(folder.urn)
+    end
+  end
+
+  describe "folder?" do
+    it "returns true if asset is a folder false otherwise" do
+      folder.folder?.should be_true
+    end
   end
 end
