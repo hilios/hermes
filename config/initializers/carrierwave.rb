a3f32b7e89336f82637aa652cf29da0215a00f4a
Edson Hilios <ehilios@corp.hotwords.com>
2012-08-10 12:39:16 -0300

    Revert "Updating the carrierwave to mongoid 3, but still in beta support"
    This reverts commit d6c5e28b32842187d20a4666971eda8138ab8b9e.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,10 @@
 require 'carrierwave/processing/mime_types'
 
 CarrierWave.configure do |config|
-  config.grid_fs_database = Mongoid::Config.sessions[:default][:database]
-  config.grid_fs_host     = Mongoid::Config.sessions[:default][:hosts].first
-  config.storage :grid_fs
+  # p Mongoid.default_session.cluster
+
+  # config.grid_fs_database = Mongoid.database
+  # config.grid_fs_host     = Mongoid.config.master.connection.host
+  # config.storage :grid_fs
   # config.grid_fs_access_url = "/image/show"
 end
