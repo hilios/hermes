0ecdf63678ac918dac4b63641643b5f65e2b6804
Edson Hilios <ehilios@corp.hotwords.com>
2012-08-10 12:41:11 -0300

    Custom bootstrap require

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,10 +2,8 @@
 require 'carrierwave/processing/mime_types'
 
 CarrierWave.configure do |config|
-  # p Mongoid.default_session.cluster
-
-  # config.grid_fs_database = Mongoid.database
-  # config.grid_fs_host     = Mongoid.config.master.connection.host
-  # config.storage :grid_fs
+  config.grid_fs_database = Mongoid.database.name
+  config.grid_fs_host     = Mongoid.config.master.connection.host
+  config.storage :grid_fs
   # config.grid_fs_access_url = "/image/show"
 end
